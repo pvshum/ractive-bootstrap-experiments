9de4681dd44e430792978dcc699c5046dcd4cc4a
Pavel <pv.shum@gmail.com>
2015-02-23 21:48:52 +1100

    - Adding basic node express server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var public_folder = '';
+
+// Default folder is the public sub-folder.
+var cfg = {
+    port: 4080
+};
+
+app.use(express.static(__dirname + '/' + public_folder));
+
+app.listen(cfg.port);
+
+console.log('Started on http://localhost:' + cfg.port);

4a1fab575fd8b043974100e40382b1f4fa63f554
Pavel <pv.shum@gmail.com>
2015-02-23 22:09:25 +1100

    - Copying vendor assets to static

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
     grunt.initConfig({
         jshint: {
             files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
@@ -11,7 +13,24 @@ module.exports = function(grunt) {
         },
         watch: {
             files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
+            tasks: ['jshint'],
+            ra: {
+                files: 'static/**/*.*',
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    {expand: true, cwd: 'node_modules', src: ['ractive/*'], dest: 'static/vendors/'},
+                    {expand: true, cwd: 'node_modules/bootstrap/dist', src: ['**/*.*'], dest: 'static/vendors/bootstrap'}
+
+                ]
+            }
         }
     });
 
